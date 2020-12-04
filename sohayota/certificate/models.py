e1945778b667b34140e623d8b1de91d65c7ab70b
07kamrul <mdkamrulhasanewu@gmail.com>
2020-12-04 22:42:25 +0600

    generate and get image

--- sohayota/certificate/models.py
+++ sohayota/certificate/models.py
@@ -2,6 +2,9 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 # Create your models here.
+from PIL import Image, ImageDraw, ImageFont
+import os
+
 
 class Sohayota(models.Model):
 
@@ -20,9 +23,31 @@ class Sohayota(models.Model):
     address = models.CharField(max_length= 200, null= True)
     occupation = models.CharField(max_length = 100, null = True, choices=OCCUPATION_STATUS)
 
-    # images = models.ImageField(default="certificate.jpg", null=True, blank=True)
+    image = models.ImageField(blank=True)
 
     def __int__(self):
         return self.id
 
+    def save(self, *args, **kwargs):
 
+        i = self.name
+        im = Image.open(r'F:\Python\Projects\Certificate\sohayota\static\images\certificate.jpg')
+        d = ImageDraw.Draw(im)
+        location = (1400, 720)
+        # text_color = (0, 137, 209)
+        text_color = ("#0C0C98")
+        font = ImageFont.truetype("calibri.ttf", 100)
+        d.text(location, i, fill=text_color, font=font)
+        im.save("F:\Python\Projects\Certificate\sohayota\static\images\certificate_" + i + ".png")
+        # os.startfile("certificate_" + i + ".png")
+
+        super(Sohayota,self).save(*args,**kwargs)
+
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
--- sohayota/certificate/views.py
+++ sohayota/certificate/views.py
@@ -21,33 +21,17 @@ def createCertificate(request):
 
         if form.is_valid():
             form.save()
+
+            obj = form.save(commit=False)
+
+            obj.image = "certificate_" + obj.name + ".png"
+            obj.save()
+
             sohayota = Sohayota.objects.latest('id')
             lastId = sohayota.id
-            print("****************************")
-            print(sohayota)
-            print(lastId)
 
-            obj = Sohayota.objects.get(id=lastId)
+            return redirect('showCertificate',lastId)
 
-            name = obj.name
-            mobileNo = obj.mobile_no
-            print("////////////////////////")
-            print(name)
-            print(mobileNo)
-
-            i = name
-
-            im = Image.open(r'F:\Python\Projects\Certificate\sohayota\static\images\certificate.jpg')
-            d = ImageDraw.Draw(im)
-            location = (1400, 720)
-            # text_color = (0, 137, 209)
-            text_color = ("#0C0C98")
-            font = ImageFont.truetype("calibri.ttf", 100)
-            d.text(location, i, fill=text_color, font=font)
-            im.save("F:\Python\Projects\Certificate\sohayota\static\images\certificate_" + i + ".png")
-            # os.startfile("certificate_" + i + ".png")
-            # return redirect('showCertificate',lastId)
-            # return showCertificate(sohayota)
 
     context = {'form':form}
 
@@ -57,23 +41,6 @@ def createCertificate(request):
 def showCertificate(request,pk_test):
     certificate = Sohayota.objects.get(id=pk_test)
 
-    name = certificate.name
-    mobileNo = certificate.mobile_no
-    print("////////////////////////")
-    print(name)
-    print(mobileNo)
+    context = {'certificate':certificate}
 
-    i = name
-
-    im = Image.open(r'certificate.png')
-
-    d = ImageDraw.Draw(im)
-    location = (250, 650)
-    text_color = (0, 137, 209)
-    font = ImageFont.truetype("calibri.ttf", 70)
-    d.text(location, i, fill=text_color, font=font)
-    im.save("certificate_" + i + ".png")
-    # os.startfile("certificate_" + i + ".png")
-
-    return render(request,"certificate/index.html",{'certificate':certificate})
-
+    return render(request,"certificate/showCertificate.html",context)
